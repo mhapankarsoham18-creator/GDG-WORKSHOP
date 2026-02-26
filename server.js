3d7511af894b91c8719fafcb0f94bd8f975209fc
Soham Mhapankar <mhapankar.soham18@gamil.com>
2026-02-26 10:21:19 +0530

    Add root server.js entry point and update package scripts

---
+++ server.js
@@ -0,0 +1,7 @@
+/**
+ * Render Entry Point
+ * This file allows Render to find and start the backend server 
+ * now that it has been moved to the /backend folder.
+ */
+
+require('./backend/server.js');
